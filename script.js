6f683b77949a02aeb3d307de76b24b40701c797b
cynthiaobia <cynthia.obia@gmail.com>
2021-09-23 00:26:15 -0400

    trying game function with one round

--- script.js
+++ script.js
@@ -13,31 +13,59 @@ function computerPlay(){
 //Play a single round
 let playerSelection = "scIssOrs";
 let computerSelection;
+let playerWins;
+let computerWins;
+let tieWins;
 
 function playRound(playerSelection, computerSelection){
     computerSelection = computerPlay();
     if (playerSelection.toLowerCase() === computerSelection){
+        tie = true;
         return "It's a tie!" ;
     } else if (playerSelection.toLowerCase() === "rock"){
         if (computerSelection === "scissors"){
+          playerWins = true;
           return "You win! Rock beats scissors!"
         } 
         else if (computerSelection === "paper"){
+          computerWins = true;
           return "Computer wins! Paper beats scissors!"
         }
     } else if (playerSelection.toLowerCase() === "paper"){
         if (computerSelection === "rock"){
+          playerWins = true;
           return "You win! Paper beats rock!"
         } 
         else if (computerSelection === "scissors"){
+          computerWins = true;
           return "Computer wins! Scissors beats paper!"
         }
     } else if (playerSelection.toLowerCase() === "scissors"){
         if (computerSelection === "paper"){
+          playerWins = true;
           return "You win! Scissors beats paper!"
         } 
         else if (computerSelection === "rock"){
+          computerWins = true;
           return "Computer wins! Rock beats scissors!"
         }
     }
+}
+
+//Game with multiple rounds
+
+let playerScore = 0;
+let computerScore = 0;
+let tie = 0;
+
+function game(){
+  playRound(playerSelection, computerSelection);
+  if (playerWins === true){
+    playerScore += 1;
+  } else if (computerWins === true){
+    computerScore += 1;
+  } else if (tie === true){
+    tie += 1;
+  }
+  return ("You: " + playerScore + ", Computer: " + computerScore + ", Tie: " + tie)
 }
